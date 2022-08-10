507b2b6edd57a4d63c81a20ff4b1a0d96131d8f4
faeezs <faeezs@gmail.com>
2022-08-10 16:26:46 +0700

    Added Employees and Customers pages

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+  Sort,
+  ContextMenu,
+  Filter,
+} from "@syncfusion/ej2-react-grids";
+
+import { ordersData, cotextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
+import { pdfExport } from "@syncfusion/ej2/pivotview";
 
 const Orders = () => {
   return (
-    <div>
-      
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
